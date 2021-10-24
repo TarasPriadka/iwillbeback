4028ef66ea75ae7c36f986b6ad5ad2c595095c11
Valeriy Rotan <val.rotan@gmail.com>
2021-10-23 22:43:21 -0700

    twilio start walk message integration

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,14 +1,16 @@
 const logger = require('../utils/logger');
 const shortid = require('shortid')
+const twilio = require('../utils/twilio')
 
-sessions = {}
+let sessions = {};
 const start = async (req, res) => {
     const body = req.body
-    logger.info(`Request body: ${body}`)
-    const sessionid =  shortid.generate()
-    sessions[sessionid] = {...body}
-    res.send({"sessionid": `${sessionid}`})
-    logger.info(sessions)
+    logger.info(`Request body: ${body}`);
+    const sessionid =  shortid.generate();
+    sessions[sessionid] = {...body};
+    res.send({"sessionid": `${sessionid}`});
+    logger.info(sessions);
+    twilio.send_start_message(body.trusteePhone, body.name, body.message, body.when, `http://illbeback.tech/trustee/${sessionid}`);
 }
 
 const verify = async (req, res) => {
--- server/src/utils/twilio.js
+++ server/src/utils/twilio.js
@@ -1,3 +1,4 @@
+// const {error} = require("server/src/utils/logger");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -12,20 +13,22 @@ function send_message(to_phone, content) {
             to: to_phone,
             from: TWLIO_PHONE,
         })
-        .then((message) => console.log(message.sid));
+        .catch(m => console.error(m))
+        .then((message) => console.log(message.sid))
+}
+
+function send_start_message(trustee_phone, walker_name, walker_action, walker_eta, url) {
+    send_message(
+        trustee_phone,
+        `I'll Be Back Alert: ${walker_name} is going to ${walker_action} expecting to be back by ${walker_eta}: ${url}`
+    )
 }
 
 function send_template_message(trustee_phone, walker_name, url) {
-    if (walker_name.length > 16) {
-        walker_name = walker_name.slice(0,13) + '...'
-    }
-    client.messages
-        .create({
-            body: `I'll Be Back Alert: ${walker_name} may be in trouble and they chose to trust you. Please go to this link: ${url}`,
-            to: trustee_phone,
-            from: TWLIO_PHONE,
-        })
-        .then((message) => console.log(message.sid));
+   send_message(
+        trustee_phone,
+        `I'll Be Back Alert: ${walker_name} may be in trouble. Please go to this link: ${url}`
+    )
 }
 
-module.exports = {send_message, send_template_message}
+module.exports = {send_message, send_start_message, send_template_message}
