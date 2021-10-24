05a33cd27a548f98dbdb1e3d7022383662551753
Ryan Teoh <ryanteoh01@gmail.com>
2021-10-23 20:37:25 -0700

    Integrating frontend with backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const { json, urlencoded } = require('body-parser')
-require('express-async-errors')
+const express = require("express");
+const { json, urlencoded } = require("body-parser");
+const cors = require("cors");
+require("express-async-errors");
 
-const app = express()
-const middleware = require('./utils/middleware')
-const { start, verify } = require('./routes/api')
+const app = express();
+const middleware = require("./utils/middleware");
+const { start, verify } = require("./routes/api");
 
-app.disable('x-powered-by')
-app.use(json())
-app.use(urlencoded({ extended: true }))
-app.use(middleware.requestLogger)
+app.disable("x-powered-by");
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(middleware.requestLogger);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/index.html");
+// });
 // app.use('/', api)
 // app.use('/api', protect)
-app.post('/api/start', start)
-app.post('/api/verify', verify)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
-module.exports = app
+app.use(express.static("build"));
+app.post("/api/start", start);
+app.post("/api/verify", verify);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
