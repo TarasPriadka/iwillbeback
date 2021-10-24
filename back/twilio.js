07a9520923d611ffdc1f6c0b20621b56572e6235
Valeriy Rotan <val.rotan@gmail.com>
2021-10-23 17:41:17 -0700

    twilio 2

--- back/twilio.js
+++ back/twilio.js
@@ -1,12 +1,29 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
+const TWLIO_PHONE = '+13253089534'
+
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-    .create({
-        body: 'Hello from Node',
-        to: '+14089312317', // Text this number
-        from: '+13253089534', // From a valid Twilio number
-    })
-    .then((message) => console.log(message.sid));
+function send_message(to_phone, content) {
+    client.messages
+        .create({
+            body: content,
+            to: to_phone,
+            from: TWLIO_PHONE,
+        })
+        .then((message) => console.log(message.sid));
+}
+
+function send_template_message(trustee_phone, walker_name, url) {
+    if (walker_name.length > 16) {
+        walker_name = walker_name.slice(0,13) + '...'
+    }
+    client.messages
+        .create({
+            body: `I'll Be Back Alert: ${walker_name} may be in trouble and they chose to trust you. Please go to this link: ${url}`,
+            to: trustee_phone,
+            from: TWLIO_PHONE,
+        })
+        .then((message) => console.log(message.sid));
+}
