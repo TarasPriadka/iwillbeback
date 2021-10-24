3b2c40b7780dcf3924017fbf3c68caee152b4440
Taras Priadka <taraspriadka@berkeley.edu>
2021-10-23 17:06:03 -0700

    Fixing UI forms.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./App.css";
 
 import AppNavbar from "./components/AppNavbar";
-import {iguanaClickedAtom} from "./atoms";
-import {useRecoilValue} from "recoil";
-import {Form, Button} from "react-bootstrap";
-
+import {Button} from "react-bootstrap";
+import TrusteeForm from "./components/TrusteeForm";
+import {goingOutAtom} from "./atoms";
+import {useRecoilState} from "recoil";
 
 function App() {
 
+    const [goingOut, setGoingOut] = useRecoilState(goingOutAtom);
+
     return <div className="">
         <AppNavbar/>
-        <Form>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Enter Trustee Name</Form.Label>
-                <Form.Control type="phone" placeholder="Enter Name:" />
-            </Form.Group>
+        <Button onClick={()=> {
+            setGoingOut(!goingOut)
+        }}>{goingOut ? "Stop Going Out" : "Go out"}</Button>
+        { goingOut ?
+            <TrusteeForm/> : <></>
+        }
 
-            <Form.Group className="mb-3" controlId="phoneNumber">
-                <Form.Label>Enter Trustee Phone Number</Form.Label>
-                <Form.Control type="phone" placeholder="Enter Phone:" />
-            </Form.Group>
-
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
     </div>
 }
 
---
+++ src/components/TrusteeForm.js
@@ -0,0 +1,53 @@
+import React, {useState} from "react";
+import "../App.css";
+import {Button, Form} from "react-bootstrap";
+import {goingOutAtom} from "../atoms";
+import {useRecoilState} from "recoil";
+
+function TrusteeForm() {
+
+    const [goingOut, setGoingOut] = useRecoilState(goingOutAtom);
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [where, setWhere] = useState("");
+    const [when, setWhen] = useState("");
+
+    const handleSubmit = (e) => {
+        console.log(
+            name,phone,where,when
+        )
+        setGoingOut(false);
+        e.preventDefault();
+    }
+
+    return <div className="trust-form m-1 p-3">
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+                <Form.Text>Trustee Info:</Form.Text>
+                <Form.Control className="mb-1" type="name" placeholder="Trustee Name:" value={name} onChange={(e) => {
+                    setName(e.target.value)
+                }}/>
+                <Form.Control className="mb-1" type="phone" placeholder="Trustee Phone Number:" value={phone}
+                              onChange={(e) => {
+                                  setPhone(e.target.value)
+                              }}/>
+                <Form.Text>Out info:</Form.Text>
+                <Form.Control className="mb-1" type="phone" placeholder="When are you coming back?"
+                              value={when}
+                              onChange={(e) => {
+                                  setWhen(e.target.value)
+                              }}/>
+                <Form.Control className="mb-1" type="phone" placeholder="Where are you coming back?"
+                              value={where}
+                              onChange={(e) => {
+                                  setWhere(e.target.value)
+                              }}/>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form></div>
+}
+
+export default TrusteeForm;
