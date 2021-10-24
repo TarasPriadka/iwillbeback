4f2b2f5c64b19a531ce05f3087db9045f7bf8ac6
Taras Priadka <taraspriadka@berkeley.edu>
2021-10-23 22:18:07 -0700

    Dotenv setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 
+require('dotenv').config()
 ReactDOM.render(
     <React.StrictMode>
         <RecoilRoot>
--- src/screens/WalkingScreen.jsx
+++ src/screens/WalkingScreen.jsx
@@ -9,18 +9,27 @@ import {useRecoilValue} from "recoil";
 import SimpleMap from "../components/Map";
 import AppNavbar from "../components/WalkerSetup/AppNavbar";
 import {Button, ButtonGroup} from "react-bootstrap";
+import {postData} from "../utils";
 
 function WalkingScreen() {
     const id = useParams();
     const loggedIn = useRecoilValue(loggedInAtom);
+
+    // const handleDanger = () => {
+    //     postData("/api/danger", {})
+    // }
+    //
+    // const handleEnd = () => {
+    //     postData("/api/end", {})
+    // }
 
     return <div>
         <AppNavbar/>
         {loggedIn ? <>
             <SimpleMap />
             <div className="m-2">
-                <Button variant="danger">Notify Trustee</Button> {' '}
-                <Button className="float-end" variant="light">End</Button>
+                <Button variant="danger" onClick={handleDanger}>Notify Trustee</Button> {' '}
+                <Button className="float-end" variant="light" onClick={handleEnd}>End</Button>
             </div>
         </> : <>
             <Login/>
